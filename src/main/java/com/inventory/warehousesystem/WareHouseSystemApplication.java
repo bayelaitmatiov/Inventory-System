3f5e69fbd0b9f906a81e935ea195a1cde69d3c90
bayelaitmatiov <baielbaktybekovich@gmail.com>
2026-04-14 19:23:47 +0600

    initial project setup

---
+++ src/main/java/com/inventory/warehousesystem/WareHouseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.warehousesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareHouseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WareHouseSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inventory/warehousesystem/WareHouseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.warehousesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareHouseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
